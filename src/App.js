bff191557f7830c392075fbf19ad7d9653e9a8b6
mitchytic <michaelchompchomp@gmail.com>
2024-03-07 01:40:56 -0500

    React Setup and Hamburger Menu Fix Set up react properly by running CRA and copy pasting everything. Create code in App.js to make a 'home screen' that is just a hamburger menu for now. Fix css for hamburger menu so it looks good when you click on it.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HamburgerMenu from './hamburgermenu'
+
+function App() {
+    return (
+        <div className="App">
+            <HamburgerMenu />
+        </div>
+    )
+}
+
+export default App;
