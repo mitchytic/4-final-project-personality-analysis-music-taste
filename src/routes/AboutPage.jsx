68a153a88ed8fd7f28a4607e2dadba5f778e87ea
Zhuang Liu <zl3889@nyu.edu>
2024-03-27 13:26:51 -0400

    fix small errors

--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './AboutPage.css';
 import HamburgerMenu from '../components/common/hamburgermenu';
+import { useNavigate } from 'react-router-dom'; 
 
 const AboutPage = () => {
+  const navigate = useNavigate(); 
+
+  const goBackToAccountSettings = () => {
+    navigate('/settings');  
+  };
+
   return (
     <>
       <HamburgerMenu />
       <div className="about-page">
         <p>We are a company that sells things!!!</p>
+
+        <button className="back-button" onClick={goBackToAccountSettings}>
+        Back
+        </button>
+
       </div>
     </>
     
