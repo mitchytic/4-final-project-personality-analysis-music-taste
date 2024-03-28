686a73ae8de6c11e63b8ac1b733579f281fa5251
SpencerWPak <spencerpak9999@gmail.com>
2024-03-28 01:23:03 -0400

    fixed

--- back-end/userdbfunctions.js
+++ back-end/userdbfunctions.js
@@ -1,57 +1,74 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-
+const fs = require('fs');
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
-const uri = ""; // Your MongoDB URI
-const client = new MongoClient(uri, {
-  tlsAllowInvalidCertificates: true,
-  serverApi: ServerApiVersion.v1
-});
+const dataPath = './accounts.json'; // Path to the JSON file
 
-const databaseName = "account";
-const collectionName = "Username+Password";
-let collection;
+// Utility function to read accounts from the JSON file
+function readAccounts() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
+  });
+}
 
-async function connectToMongo() {
-  try {
-    await client.connect();
-    const database = client.db(databaseName);
-    collection = database.collection(collectionName);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Could not connect to MongoDB:', error);
-  }
+// Utility function to write accounts to the JSON file
+function writeAccounts(accounts) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(dataPath, JSON.stringify(accounts, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 }
 
 app.post('/add-user', async (req, res) => {
   const { username, password } = req.body;
-  if (await collection.findOne({ username })) {
+  const accounts = await readAccounts();
+  if (accounts.some(account => account.username === username)) {
     return res.status(400).send({ message: 'User already exists' });
   }
-  await collection.insertOne({ username, password });
+  accounts.push({ username, password, "Music Taste": 1 }); // Adding with default "Music Taste"
+  await writeAccounts(accounts);
   res.send({ message: 'User added successfully' });
 });
 
 app.post('/check-user', async (req, res) => {
   const { username, password } = req.body;
-  const user = await collection.findOne({ username, password });
+  const accounts = await readAccounts();
+  const user = accounts.find(account => account.username === username && account.password === password);
   res.send({ exists: !!user });
 });
 
 app.delete('/delete-user', async (req, res) => {
   const { username, password } = req.body;
-  const result = await collection.deleteOne({ username, password });
-  if (result.deletedCount > 0) {
+  let accounts = await readAccounts();
+  const initialLength = accounts.length;
+  accounts = accounts.filter(account => !(account.username === username && account.password === password));
+  if (accounts.length < initialLength) {
+    await writeAccounts(accounts);
     res.send({ message: 'User deleted successfully' });
   } else {
     res.status(404).send({ message: 'User not found' });
   }
 });
 
-connectToMongo().then(() => {
-  app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+app.post('/check-user', async (req, res) => {
+  const { username, password } = req.body;
+  const accounts = await readAccounts();
+  const user = accounts.find(account => account.username === username && account.password === password);
+  res.send({ exists: !!user });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
